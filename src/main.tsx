d690b1eec9d2506d5b3dcb6b89e93cf33bfbea9d
Eishal <eishal.cecos@gmail.com>
2026-08-16 02:24:45 -0700

    Load Ashes performance optimizations

--- src/main.tsx
+++ src/main.tsx
@@ -3,4 +3,5 @@ import { createRoot } from 'react-dom/client';
 import App from './App';
 import './index.css';
 import './card-image-size.css';
+import './performance.css';
 createRoot(document.getElementById('root')!).render(<StrictMode><App/></StrictMode>);

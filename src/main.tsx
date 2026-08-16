63f9e795de648b90324bb5b54ba308d67ff9d965
Eishal <eishal.cecos@gmail.com>
2026-08-16 02:23:01 -0700

    Load larger capability card images

--- src/main.tsx
+++ src/main.tsx
@@ -2,4 +2,5 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App';
 import './index.css';
+import './card-image-size.css';
 createRoot(document.getElementById('root')!).render(<StrictMode><App/></StrictMode>);
